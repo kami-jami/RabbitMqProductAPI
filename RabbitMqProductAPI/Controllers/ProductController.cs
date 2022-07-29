67a9c5a513e3643906e8efabefa25fdc2c7edf6c
a.jami <kami.jami@gmail.com>
2022-07-30 00:46:16 +0430

    Added Services to project -product and rabbitMQ services

---
+++ RabbitMqProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMqProductAPI.Models;
+using RabbitMqProductAPI.RabbitMQ;
+using RabbitMqProductAPI.Services;
+
+namespace RabbitMqProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IRabitMQProducer _rabitMQProducer;
+
+        public ProductController(IProductService productService, IRabitMQProducer rabitMQProducer)
+        {
+            _productService = productService;
+            _rabitMQProducer = rabitMQProducer;
+        }
+
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
+        {
+            var productList = _productService.GetProductList();
+            return productList;
+        }
+
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int id)
+        {
+            return _productService.GetProductById(id);
+        }
+
+        [HttpPost("addproduct")]
+        public Product AddProduct(Product product)
+        {
+            var productData = _productService.AddProduct(product);
+
+            _rabitMQProducer.SendProductMessage(productData);
+            return productData;
+        }
+
+        [HttpPut("updateproduct")]
+        public Product UpdateProduct(Product product)
+        {
+            return _productService.UpdateProduct(product);
+        }
+
+        
+
+
+    }
+}
